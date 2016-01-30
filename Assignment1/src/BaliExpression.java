3bc56ac929f89fcde07b1943bb59bf712e99d128
Karim <kfserhan@gmail.com>
2016-01-30 16:21:37 -0600

    asdfkarim

--- Assignment1/src/BaliExpression.java
+++ Assignment1/src/BaliExpression.java
@@ -1,12 +1,13 @@
 import edu.cornell.cs.sam.io.SamTokenizer;
+import edu.cornell.cs.sam.io.Tokenizer;
 
 public class BaliExpression {
 
-    public static String getExp(SamTokenizer f)
-    {
+    public static String getExp(SamTokenizer f) {
         switch (f.peekAtKind()) {
             case INTEGER: //E -> integer
                 return "PUSHIMM " + f.getInt() + "\n";
+            case
             case OPERATOR:
             {
             }
