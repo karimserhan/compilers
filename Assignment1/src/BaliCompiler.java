fc929d10b7efe3f0b543ed6765fcc5da4bb99b43
Elie Antoun <egantoun@wireless-10-146-207-181.public.utexas.edu>
2016-01-30 14:51:38 -0600

    Declarations

--- Assignment1/src/BaliCompiler.java
+++ Assignment1/src/BaliCompiler.java
@@ -1,25 +1,26 @@
 import edu.cornell.cs.sam.io.SamTokenizer;
 import edu.cornell.cs.sam.io.Tokenizer;
 import edu.cornell.cs.sam.io.Tokenizer.TokenType;
+import edu.cornell.cs.sam.io.TokenizerException;
 
 public class BaliCompiler
 {
-	static String compiler(String fileName) 
+	public static String compiler(String fileName)
 	{
 		//returns SaM code for program in file
-		try 
+		try
 		{
 			SamTokenizer f = new SamTokenizer (fileName);
 			String pgm = getProgram(f);
 			return pgm;
-		} 
-		catch (Exception e) 
+		}
+		catch (Exception e)
 		{
 			System.out.println("Fatal error: could not compile program");
 			return "STOP\n";
 		}
 	}
-	static String getProgram(SamTokenizer f)
+	public static String getProgram(SamTokenizer f)
 	{
 		try
 		{
@@ -34,16 +35,16 @@ public class BaliCompiler
 		{
 			System.out.println("Fatal error: could not compile program");
 			return "STOP\n";
-		}		
+		}
 	}
-	static String getMethod(SamTokenizer f)
+	public static String getMethod(SamTokenizer f)
 	{
 		//TODO: add code to convert a method declaration to SaM code.
 		//Since the only data type is an int, you can safely check for int 
 		//in the tokenizer.
 		//TODO: add appropriate exception handlers to generate useful error msgs.
 		f.check("int"); //must match at begining
-		String methodName = f.getString(); 
+		String methodName = f.getString();
 		f.check ("("); // must be an opening parenthesis
 		String formals = getFormals(f);
 		f.check(")");  // must be an closing parenthesis
@@ -55,18 +56,18 @@ public class BaliCompiler
 		//And then have calls to getDeclarations and getStatements.
 		return null;
 	}
-	static String getExp(SamTokenizer f) 
+	public static String getExp(SamTokenizer f)
 	{
 			  switch (f.peekAtKind()) {
 				 case INTEGER: //E -> integer
 					return "PUSHIMM " + f.getInt() + "\n";
-				 case OPERATOR:  
+				 case OPERATOR:
 				 {
 				 }
 				 default:   return "ERROR\n";
 			  }
 	}
-	static String getFormals(SamTokenizer f){
+	public static String getFormals(SamTokenizer f){
 
 		while(f.peekAtKind() != TokenType.CHARACTER) {
 			f.check("int");
@@ -78,7 +79,64 @@ public class BaliCompiler
 		}
 		return null;
 	}
-	static String getBody(SamTokenizer f) {
+	public static String getBody(SamTokenizer f)
+	{
+		return null;
+
+	}
+
+	public static String getDeclaration(SamTokenizer f) {
+
+		try
+		{
+			f.check("int");
+
+			while(true)
+			{
+				String variableName;
+				try {
+					variableName = f.getWord();
+				}
+				catch (TokenizerException e)
+				{
+					//Error
+					//Invalid variable name
+
+					return null;
+				}
+				if(f.check("=")) {
+					String expression = getExp(f);
+				}
+				else {
+					f.pushBack();
+				}
+
+				if(!f.check(",")) {
+					f.pushBack();
+					break;
+				}
+			}
+
+			if(!f.check(";"))
+			{
+				//Error
+				//Statment must end with semicolon
+				return null;
+			}
+
+
+			//Need to change
+			return null;
+		}
+
+		catch (Exception e)
+		{
+			//
+		}
+
+		return null;
+
 
 	}
 }
+
