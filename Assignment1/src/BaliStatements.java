cc7ebb7e9c2de88327e4fa8299a0e0d0c9711dbc
Karim <kfserhan@gmail.com>
2016-01-30 15:07:19 -0600

    asdf

--- Assignment1/src/BaliStatements.java
+++ Assignment1/src/BaliStatements.java
@@ -19,6 +19,8 @@ public class BaliStatements {
         } else if (f.check("{")) {
             f.pushBack();
             getBlock(f);
+        }else if (f.check(";")) {
+            // do nothing
         } else { // hopefully an ASSIGN
             f.pushBack();
             getAssign(f);
@@ -42,22 +44,76 @@ public class BaliStatements {
     }
 
     public static String getBlock(SamTokenizer f) {
+        if (!f.check("{")) {
+            //error
+            return null;
+        }
+
+        while (!f.check("}")) {
+            f.pushBack();
+            getStatement(f);
+        }
         return null;
     }
 
     public static String getBreak(SamTokenizer f) {
+        f.check("break");
+
+        if (!f.check(";")) {
+            // error
+            return null;
+        }
         return null;
     }
 
     public static String getWhile(SamTokenizer f) {
+        f.check("while");
+
+        if (!f.check("(")) {
+            // error
+            return null;
+        }
+        BaliExpression.getExp(f);
+
+        if (!f.check(")")) {
+            // error
+            return null;
+        }
+        getStatement(f);
         return null;
     }
 
     public static String getIf(SamTokenizer f) {
+        f.check("if");
+
+        if (!f.check("(")) {
+            // error
+            return null;
+        }
+        BaliExpression.getExp(f);
+
+        if (!f.check(")")) {
+            // error
+            return null;
+        }
+        getStatement(f);
+
+        if (!f.check("else")) {
+            // error
+            return null;
+        }
+
+        getStatement(f);
         return null;
     }
 
     public static String getReturn(SamTokenizer f) {
+        f.check("return");
+        BaliExpression.getExp(f);
+        if (!f.check(";")) {
+            // error
+            return null;
+        }
         return null;
     }
 }
