de6cb1a767e3702b7d99bf6891f21d93970ab80e
Elie Antoun <egantoun@wireless-10-146-207-181.public.utexas.edu>
2016-01-30 16:22:10 -0600

    asda

--- Assignment1/src/BaliExpression.java
+++ Assignment1/src/BaliExpression.java
@@ -1,12 +1,50 @@
 import edu.cornell.cs.sam.io.SamTokenizer;
+import edu.cornell.cs.sam.io.Tokenizer;
+import edu.cornell.cs.sam.io.TokenizerException;
 
 public class BaliExpression {
 
     public static String getExp(SamTokenizer f)
     {
+        if(f.peekAtKind() == Tokenizer.TokenType.INTEGER)
+        {
+            //Literal Case
+        }
+        else if(f.check("true"))
+        {
+            f.pushBack();
+            //Literal Case
+        }
+
+        else
+        {
+            //Method or Location
+            try
+            {
+                String variableName = f.getWord();
+            }
+            catch (TokenizerException e)
+            {
+                //Error
+                //Wrong name
+                return null;
+            }
+            //Check if next token is open paran
+            if(f.check("("))
+            {
+                //case method
+            }
+
+
+        }
+
+
+
+
         switch (f.peekAtKind()) {
             case INTEGER: //E -> integer
                 return "PUSHIMM " + f.getInt() + "\n";
+
             case OPERATOR:
             {
             }
